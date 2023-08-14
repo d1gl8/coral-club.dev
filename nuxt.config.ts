f89de4cd4fb2957634108fdda50c50a172f745e5
dglshkv <dglshkv@gmail.com>
2023-08-14 20:15:51 +0300

    styles basis

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  css: ["~/assets/styles/main.scss"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @import "@/assets/styles/_variables.scss";
+            @import "@/assets/styles/_mixins.scss";
+          `,
+        },
+      },
+    },
+  },
 });
